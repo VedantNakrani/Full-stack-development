f2cd7853c04d9d2039bc0b15c0e5c1120671c883
Vedant Nakrani <71334813+VedantNakrani@users.noreply.github.com>
2020-09-15 22:17:24 +0530

    Update index.js

--- Part1/courseinfo/index.js
+++ Part1/courseinfo/index.js
@@ -9,19 +9,43 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part1 = (props) => {
   return (
     <div>
       <p>
         {props.part1} {props.exercises1}
       </p>
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+  return (
+    <div>
       <p>
         {props.part2} {props.exercises2}
       </p>
+    </div>
+  )
+}
+
+const Part3 = (props) => {
+  return (
+    <div>
       <p>
         {props.part3} {props.exercises3}
       </p>
-      </div>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part1 part1 = {props.part1} exercises1 = {props.exercises1}/>
+      <Part2 part2 = {props.part2} exercises2 = {props.exercises2}/>
+      <Part3 part3 = {props.part3} exercises3 = {props.exercises3}/>
+    </div>
   );
 }
 
@@ -58,3 +82,4 @@ const App = () => {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+

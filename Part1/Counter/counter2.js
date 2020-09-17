cc4b1c68b8b9e34a5438d8a082b8c69638481f95
Vedant Nakrani <71334813+VedantNakrani@users.noreply.github.com>
2020-09-17 18:56:53 +0530

    Update counter2.js
    directly updated clicks inside setClicks()

--- Part1/Counter/counter2.js
+++ Part1/Counter/counter2.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   
-  const [clicks, setClicks] = useState({ left: 0, right: 0})
+  const [clicks, setClicks] = useState({ left: 0, right: 0 })
   
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1
-    }
-    setClicks(newClicks)
+    setClicks({ ...clicks, left: clicks.left + 1 })
   }
 
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+    setClicks({ ...clicks, right: clicks.right + 1 })
   }
   
   return (

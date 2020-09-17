374395f35105ad9fc41d81d2736bda824372d6f8
Vedant Nakrani <71334813+VedantNakrani@users.noreply.github.com>
2020-09-17 21:30:58 +0530

    Update feedback.js
    Added average, total and positive reviews

--- Part1/Unicode/feedback.js
+++ Part1/Unicode/feedback.js
@@ -8,15 +8,23 @@ const Button = ({handleClick, text}) => (
 const App = () => {
   
   const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0})
+
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
   
   const updateGood = () => {
     setFeedback({...feedback, good: feedback.good + 1})
+    setTotal(total + 1)
+    setAverage(average + 1)
   }
   const updateNeutral = () => {
     setFeedback({...feedback, neutral: feedback.neutral + 1})
+    setTotal(total + 1)
   }
   const updateBad = () => {
     setFeedback({...feedback, bad: feedback.bad + 1})
+    setTotal(total + 1)
+    setAverage(average - 1)
   }
   
   return (
@@ -28,7 +36,10 @@ const App = () => {
       <h1>Statistics</h1>
       Good: {feedback.good}<br></br>
       Neutral: {feedback.neutral}<br></br>
-      Bad: {feedback.bad}
+      Bad: {feedback.bad}<br></br>
+      Total: {total}<br></br>
+      Average: {average/total}<br></br>
+      Positive: {feedback.good/total * 100}
     </div>
   )
 }
